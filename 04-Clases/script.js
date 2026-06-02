8a27e5c56fef4908c305191535ff5c63ed08e0dc
unknown <230435186+joseantoniohernandes07-ux@users.noreply.github.com>
2026-06-02 11:07:40 -0600

    Aprendimos sobre ClassList: agregar, comprovar y remover clases

---
+++ 04-Clases/script.js
@@ -0,0 +1,72 @@
+// Acceder al primer elemento de la lista
+
+const botana = document.querySelector(".botana");
+
+// Mostrar por consola
+
+console.log(botana);
+
+/* 
+
+  Propiedad classList
+
+  Devuelve un Dom Token List o Listado de Clases en el DOM. La lista de clases de un elemento de html.
+
+  nodo.classList
+
+*/
+
+console.log(botana.classList);
+console.log(botana.classList[1]); //semilla
+
+/* 
+
+  Podemos agregar clases usando el metodo add()
+
+  Sintaxis
+
+  nodo.classList
+
+*/
+
+const complemento = document.querySelectorAll(".botana");
+
+console.log(complemento[3]);
+
+complemento[3].classList.add("picante");
+
+console.log(complemento[3].classList);
+
+/* 
+
+  Podemos verificar si existe una claseen un elemento con el metodo contains()
+
+  Sintaxis:
+
+  nodo.classList.contains("nombre-clase");
+
+  -Devuelve "true" si existe la clase.
+  -Devuelve "false" si no existe la clase.
+
+*/
+
+console.log( complemento[3].classList.contains("picante") ); // True
+console.log( botana.classList.contains("picante") ); // False
+
+/* 
+
+  Podemos eliminar una clase usando el metodo remove();
+
+  Sintaxis:
+
+  nodo.classList.remove("nombre-clase");
+
+*/
+
+botana.classList.remove("botana");
+
+console.log(botana.classList)
+
+complemento[3].classList.remove("picante")
+
+console.log(complemento[3].classList)
